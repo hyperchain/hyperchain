263e520afed041a6f7d568cab93992445a0ee1ad
EddieFan31 <eddie_31@163.com>
2023-05-16 15:48:13 +0800

    更新V2.12介绍文档

--- docs/zh_CN/conf.py
+++ docs/zh_CN/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'hyperchain'
-copyright = u'2016-2023, Hangzhou Hyperchain Technology Co., Ltd'
+copyright = u'2016-2023, Hangzhou Hyperchain Technology Co., Ltd.'
 author = u'Hangzhou Hyperchain Technology Co., Ltd.'
 
 # The version info for the project you're documenting, acts as replacement for
